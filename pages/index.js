14395ca9b7787272fc4752d7012a1f6ff8a35f43
Shreyas Bharadwaj <s_bharadwaj@outlook.com>
2022-11-29 00:21:45 +0530

    new build

--- pages/index.js
+++ pages/index.js
@@ -33,7 +33,7 @@ const Home = () => {
   return (
     <div className="root">
       <Head>
-        <title>GPT-3 Writer | buildspace</title>
+        <title>AI Mentor</title>
       </Head>
       <div className="container">
         <div className="header">
